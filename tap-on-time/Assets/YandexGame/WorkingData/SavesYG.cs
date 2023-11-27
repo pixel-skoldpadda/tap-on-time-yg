437a1379b982cb9a09a69dc9b6b75731f3ee7ebf
Vladislav <legovladislav@gmail.com>
2023-11-27 15:37:36 +0300

    Удалил папку Exaples. Почистил SavesYG.

--- tap-on-time/Assets/YandexGame/WorkingData/SavesYG.cs
+++ tap-on-time/Assets/YandexGame/WorkingData/SavesYG.cs
@@ -9,25 +9,16 @@ namespace YG
     [Serializable]
     public class SavesYG
     {
-        // "Технические сохранения" для работы плагина (Не удалять)
         public int idSave;
         public bool isFirstSession = true;
         public string language = "ru";
         public bool promptDone;
-        
-        // Тестовые сохранения для демо сцены
-        // Можно удалить этот код, но тогда удалите и демо (папка Example)
-        public int money = 1;                       // Можно задать полям значения по умолчанию
-        public string newPlayerName = "Hello!";
-        public bool[] openLevels = new bool[3];
-
-        // Ваши сохранения
 
         [SerializeField] private int gems;
         [SerializeField] private int level;
         [SerializeField] private int score;
         [SerializeField] private int levelIndex;
-        [SerializeField] private SkinType skinType;
+        [SerializeField] private SkinType skinType = SkinType.Rocket;
 
         private Level _currentLevel;
         private bool _levelStarted;
@@ -36,23 +27,6 @@ namespace YG
         private Action _scoreChanged;
         private Action<Level> _levelChanged;
 
-        // Вы можете выполнить какие то действия при загрузке сохранений
-        public SavesYG()
-        {
-            gems = 0;
-            score = 0;
-            level = 0;
-            levelIndex = 0;
-            skinType = SkinType.Rocket;
-
-            _currentLevel = null;
-            _levelStarted = false;
-            
-            // Допустим, задать значения по умолчанию для отдельных элементов массива
-
-            openLevels[1] = true;
-        }
-        
         public SkinType SkinType
         {
             get => skinType;
