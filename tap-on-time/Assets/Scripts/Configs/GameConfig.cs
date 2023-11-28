76ed39f4ac6a7adf4215b58862a4b917802de04f
Vladislav <legovladislav@gmail.com>
2023-11-28 18:39:13 +0300

    Смена языка по клику кнопки на HUD

--- tap-on-time/Assets/Scripts/Configs/GameConfig.cs
+++ tap-on-time/Assets/Scripts/Configs/GameConfig.cs
@@ -2,6 +2,12 @@
 {
     public static class GameConfig
     {
+        // TODO либо вынести в айтемсы либо вытащить доступные языки локализации из плагина
+        public static readonly string[] Langs = {
+            "ru",
+            "en"
+        };
+
         public const string LeaderboardId = "totalScoreLeaderboard";
     }
 }
---
+++ tap-on-time/Assets/Scripts/Ui/Hud/LanguageContainer.cs
@@ -0,0 +1,41 @@
+﻿using Configs;
+using TMPro;
+using UnityEngine;
+using YG;
+
+namespace UI.Hud
+{
+    public class LanguageContainer : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI currentLanguage;
+
+        private void Start()
+        {
+            currentLanguage.text = YandexGame.savesData.language;
+        }
+
+        // TODO 100% можно оптимизировать
+        public void ChangeLanguage()
+        {
+            string currentLang = YandexGame.savesData.language;
+            string[] langs = GameConfig.Langs;
+            
+            for (var i = 0; i < langs.Length; i++)
+            {
+                if (currentLang.Equals(langs[i]))
+                {
+                    int nextLangIndex = i + 1;
+                    if (nextLangIndex > langs.Length - 1)
+                    {
+                        nextLangIndex = 0;
+                    }
+
+                    currentLang = langs[nextLangIndex];
+                    YandexGame.SwitchLanguage(currentLang);
+                    currentLanguage.text = currentLang;
+                    return;
+                }
+            }
+        }
+    }
+}
